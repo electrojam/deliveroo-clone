787f23af6a1607bf1b75971bd70af233c14e26a1
electrojam <electrojam@hotmail.com>
2022-08-18 03:01:59 -0500

    featured rows

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { TailwindProvider } from 'tailwindcss-react-native/dist/provider';
+import { TailwindProvider } from 'tailwindcss-react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
@@ -18,5 +19,6 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
 
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon } from 'react-native-heroicons/outline'
+import { TextInput } from 'react-native';
+import Categories from '../components/Categories';
+import FeaturedRow from '../components/FeaturedRow';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,8 +17,68 @@ const HomeScreen = () => {
   }, [])
 
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2" >
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru"
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">
+            Deliver Now!
+          </Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB"/>
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center sapce-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2  bg-gray-200 p-3">
+            <SearchIcon color="gray" size={20} />
+            <TextInput 
+              placeholder="Restaurants and cuisiness"
+              keyboardType="default"
+            />
+        </View>
+        <AdjustmentsIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView 
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100
+        }}
+      >
+        {/* Categories */}
+        <Categories />
+
+        {/* Featured Rows */}
+        <FeaturedRow 
+          id="123"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+        <FeaturedRow 
+          id="1234"
+          title="Tasty Discounts"
+          description="Everyone's been enjoyin these juicy discounts!"
+        />
+        <FeaturedRow 
+          id="12345"
+          title="Offers near you!"
+          description="Why not support your local restaurant ronight!"
+        />
+
+      </ScrollView>
     </SafeAreaView>
   )
 }
