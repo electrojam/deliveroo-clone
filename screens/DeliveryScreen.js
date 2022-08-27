4c0cb0a459af02be813d5126e00650be1e69d72d
electrojam <electrojam@hotmail.com>
2022-08-27 05:10:32 -0500

    Final app finish Delivery Screen

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -6,6 +6,7 @@ import { useNavigation } from '@react-navigation/native'
 import { useSelector } from 'react-redux'
 import { selectRestaurant } from '../features/restaurantSlice'
 import * as Progress from "react-native-progress"
+import MapView, { Marker } from 'react-native-maps'
 
 const DeliveryScreen = () => {
     const navigation = useNavigation()
@@ -41,6 +42,43 @@ const DeliveryScreen = () => {
               Your order at {restaurant.title} is being prepared 
             </Text>
           </View>
+        </SafeAreaView>
+
+        <MapView
+          initialRegion={{
+            latitude: restaurant.lat,
+            longitude: restaurant.long,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+          className="flex-1 -mt-10 z-0"
+          mapType="mutedStandard"          
+        >
+          <Marker 
+            coordinate={{
+              latitude: restaurant.lat,
+              longitude: restaurant.long,
+            }}
+            title={restaurant.title}
+            description={restaurant.short_description}
+            identifier="origin"
+            pinColor="#00CCBB"
+          />
+        </MapView>
+
+        <SafeAreaView className="bg-white flex-row space-x-5 h-28">
+          <Image 
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+          />
+          <View className="flex-1">
+            <Text className="text-lg">Julio Mesa</Text>
+            <Text className="text-gray-400">Your Rider</Text>
+          </View>
+
+          <Text className="text-[#00CCBB] text-lg mr-5 font-bold">Call</Text>
         </SafeAreaView>
     </View>
   )
